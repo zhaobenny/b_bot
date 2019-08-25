6c279388f30e8ce4fbec5669b7316737d25fe0e2
Benny Zhao <benny_zhao@sfu.ca>
2019-08-25 01:29:22 -0700

    Improved avatar cmd

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'avatar',
     description: 'Returns avatar image',
@@ -6,11 +8,20 @@ module.exports = {
         if (args.length != 0 && msg.mentions.users.size === 0){
             return msg.channel.send(`what are u doing thats not how u use it`);
         }
+        const embed = new Discord.MessageEmbed();
         if (msg.mentions.users.size === 0){
-            return msg.channel.send(`Your avatar link: <${msg.author.displayAvatarURL()}>`);
+            embed
+            .setImage(msg.author.displayAvatarURL())
+            .setTitle("Your avatar link" )
+            .setURL(msg.author.displayAvatarURL())
+            return msg.channel.send(embed);
         } else if (msg.mentions.users.size >= 1) {
            msg.mentions.users.map(user => {
-                return msg.channel.send(`${user.username}'s avatar link: <${user.displayAvatarURL()}>`)
+            embed
+            .setImage(msg.user.displayAvatarURL())
+            .setTitle("Your avatar link" )
+            .setURL(msg.user.displayAvatarURL())
+            return msg.channel.send(embed);
            });
         }
 	},
