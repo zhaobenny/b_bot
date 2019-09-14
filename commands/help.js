fe6f77953a8bca86f7fd2439be21e651b1ed2892
Benny Zhao <benny_zhao@sfu.ca>
2019-09-13 23:16:59 -0700

    Help cmd leads to github wiki page

--- commands/help.js
+++ commands/help.js
@@ -6,15 +6,10 @@ module.exports = {
   aliases: ['what'],
 	run(client, msg, args) {
         const embed = new Discord.MessageEmbed()
-        .setTitle("Bot Commands")
-        .setAuthor("!B", "https://cdn.discordapp.com/avatars/613858461628891252/694dc5a4dc73545fb7d4ed6c312d50ef.png?size=2048")
+        .setTitle("Click here for a list of bot commands!")
+        .setAuthor("Help", "https://cdn.discordapp.com/avatars/613858461628891252/694dc5a4dc73545fb7d4ed6c312d50ef.png?size=2048")
         .setColor(0x00AE86)
-        .addField("$help",
-          "you're already here")
-        .addField("$avatar [@someone]",
-          "returns avatar, leave blank for self")
-        .addField("$ping",
-          "returns pong");
+        .setURL("https://github.com/zhaobenny/b_bot/wiki/Commands")
 
         msg.channel.send({embed});
 	},
