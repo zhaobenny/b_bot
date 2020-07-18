c6914c8b9d0e011006282e1ee5d98feb90af8dc2
Benny Zhao <benny_zhao@sfu.ca>
2020-07-17 19:24:23 -0700

    Switch to dependabot

--- events/message.js
+++ events/message.js
@@ -1,19 +1,19 @@
 module.exports = (client, msg) => {
     if (msg.guild.id == 282615940124901377 && msg.content == "where benny"){
-        msg.channel.send("idk");
+        return msg.channel.send("idk")
     }
 
     if ((msg.content.indexOf(client.config.prefix) !== 0) || msg.author.bot){
-        return;
+        return
     }
-    const args = msg.content.substring(client.config.prefix.length).split(/ +/g);
-    const commandName = args.shift().toLowerCase();
-    const  command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));;
+    const args = msg.content.substring(client.config.prefix.length).split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    const  command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
     if (client.config.debug){
         console.log("[BOT] Attempting to run command \"" + commandName + "\" with arguments: [" + args + "]")
     }
     if (!command){
-        return msg.channel.send('wtf like thats not a command');
+        return msg.channel.send('wtf like thats not a command')
     }
     command.run(client, msg, args);
 };
