240387481e9ee1d7e4948afc3980bc9f73a05a9c
Benny Zhao <benny_zhao@sfu.ca>
2020-07-18 16:56:10 -0700

    Fix shuffle cmd

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -3,11 +3,11 @@ module.exports = {
   description: 'Shuffles queue',
   async run (client, msg, args) {
     const player = await client.music.playerCollection.get(msg.guild.id)
-    // TO DO: fix this shuffle command
     if (player && !player.queue.empty) {
       msg.react('👍')
-      if (args[0] === 'all') {
-
+      for (var i = 1; i <= player.queue.size - 1; i++){
+        await player.queue.moveTrack(i, Math.floor(Math.random() * (player.queue.size - 1)))
+      }
     } else {
       return msg.channel.send('There is no queue')
     }
