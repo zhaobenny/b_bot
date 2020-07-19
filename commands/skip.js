476ab54d3fe524a172b045679b23acf5e7b5e97d
Benny Zhao <benny_zhao@sfu.ca>
2020-07-18 17:28:36 -0700

    Fixed skip again

--- commands/skip.js
+++ commands/skip.js
@@ -7,11 +7,13 @@ module.exports = {
     if (!player) {
       return msg.channel.send('I am not in one?')
     }
-    if (player && player.queue.empty) {
-      return msg.channel.send('There is no queue')
-    }
-    if (player.playing && player.queue.size == 0) {
+
+    if (player.playing && player.queue.empty) {
       await player.destroy()
+    }
+
+    if (player.queue.empty) {
+      return msg.channel.send('There is no queue')
     } else {
       player.play()
       client.commands.get('np').run(client, msg, args)
