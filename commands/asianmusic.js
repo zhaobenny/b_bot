0284f3ba905d6da91cf0e2c867d7cf31918b0afb
Benny Zhao <benny_zhao@sfu.ca>
2020-07-18 17:28:24 -0700

    "Fixed" the concurrency issues by faking delays

--- commands/asianmusic.js
+++ commands/asianmusic.js
@@ -6,16 +6,22 @@ module.exports = {
   name: 'asianmusic',
   description: 'Play a hardcoded Kpop + Jpop music playlist from Youtube',
   aliases: ['usual', 'aznmusic'],
+
   async run (client, msg, args) {
     var player = await client.music.playerCollection.get(msg.guild.id);
     if (player && !player.queue.empty) {
       return msg.channel.send('There is already a queue')
     }
+    const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+    // Simulate delays
+
     play.run(client, msg, ['https://www.youtube.com/playlist?list=PLjGLpHMZeGzKnUIS7druCuSbgCJumFpQp'])
+    await snooze(2000);
     play.run(client, msg, ['https://www.youtube.com/playlist?list=PL1R1F6p67q33siReKhbAlbdhZZhjTch8F'])
-    // TO DO: fix these cmd
-    shuffle.run(client, msg, ['all'])
-    // skip.run(client, msg, [''])
-    return msg.channel.send("Command doesn't shuffle songs after adding - tell Benny to fix it lol")
+    await snooze(2000);
+    shuffle.run(client, msg, [''])
+    await snooze(3000);
+    skip.run(client, msg, [''])
+    return msg.react('👍')
   }
 }
